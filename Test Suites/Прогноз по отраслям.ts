07452ceb642440bf3171ee74550667ca46f3c1c2
Daria <Daria@DESKTOP-FCJ6RQD.IGD_Rostelecom>
2024-01-24 15:10:38 +0700

    24.01.2024

--- Test Suites/Прогноз по отраслям.ts
+++ Test Suites/Прогноз по отраслям.ts
@@ -11,4 +11,109 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>cc9a6181-60ea-4fec-9a52-5b9b0a6764ad</testSuiteGuid>
+   <testCaseLink>
+      <guid>b970baad-0e2e-4b9b-8ef2-753b7663b8a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям ПАО Россети и Магистральные сети</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>19e29068-e4bd-4f02-8fa5-d86c0f4cd456</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Волга</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3048ab9c-0cde-4cd1-83c0-fb9c624e7b53</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Кубань</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d05da7f8-52b3-4d88-b292-72ac2995d5f3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Ленэнерго</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7d0e182e-b25a-450f-a779-3945bce41254</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Московский регион</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>070a025d-32f8-4c33-912a-9855d1a01531</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Северный Кавказ</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d3793108-28d7-4387-8fb4-c82e6ce0ef20</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Северо-Запад</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2fbd9b2f-28e3-45d9-8b4b-fbbad7169e11</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Сибирь</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>84680067-05fe-4aa9-ba68-d10b19cc57cc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Томск</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e460e0f8-7cfa-43e6-8051-bb0497c68e83</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Тюмень</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4f7a4633-96d6-4d41-9914-b0d42c9c9e16</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Урал</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>89ecee34-0705-4ed6-9e71-a9799d94ff5c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Центр и Приволжье</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7aad567c-7662-4758-ba46-e59b797399b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Центр</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b05077af-8025-48fb-88ca-495483766417</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Юг</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5b841fa0-7cb2-422e-bb1a-0d85542d4886</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Прогноз по отраслям/Прогноз по отраслям проц 2020/Прогноз по отраслям Россети Янтарь</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
